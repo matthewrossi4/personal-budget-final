50166f21e4613726d0eae6bb538ff82d1fb07788
Matthew Rossi <mrossi4@uncc.edu>
2020-12-17 17:10:37 -0500

    Added the IP back

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -25,7 +25,7 @@ function Signup() {
         }
 
         axios
-        .post(`${process.env.SERVER_ADDR}/api/signup`, {getUsername, getPassword})
+        .post('http://54.163.146.144:4000/api/signup', {getUsername, getPassword})
         .then((res) => {
             history.push("/login");
         })
